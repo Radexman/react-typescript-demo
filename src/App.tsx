6756adf8bb71f6a68e67ab9162fbad88ab0c591a
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-10-09 13:48:13 +0200

    CSSProperties Prop Type

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
-import { log } from 'console';
 import './App.css';
-import Button from './components/Button';
-import Input from './components/props/Input';
+import Container from './components/props/Container';
 
 function App() {
 	return (
 		<div className='App'>
-			<Button
-				handleClick={(event, id) => {
-					console.log('Button Clicked', event, id);
-				}}
-			/>
-			<Input
-				value=''
-				handleChange={(event) => console.log(event)}
-			/>
+			<Container styles={{ border: '1px solid black', padding: '1rem', borderRadius: '.7em' }} />
 		</div>
 	);
 }
